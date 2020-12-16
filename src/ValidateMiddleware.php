bf7292d1dc830b2e9284fd01d8a29b4ffb4fdbd2
auooru <auooru@outlook.com>
2020-12-16 23:08:06 +0800

    update: 合并代码

--- src/ValidateMiddleware.php
+++ src/ValidateMiddleware.php
@@ -74,34 +74,43 @@ class ValidateMiddleware
         if ($annotation !== null) {
             $validateClass = $annotation->value;
             $validateScene = $annotation->scene;
-            /** @var Validate $v */
-            $v = new $validateClass();
-            if ($validateScene) {
-                // 自行决定使用何种场景
-                if ('?' === $validateScene && $v instanceof AskSceneInterface) {
-                    $validateScene = $v->askScene($request) ?: false;
-                }
-                // 选中验证场景
-                $validateScene && $v->scene($validateScene);
+            $result = $this->execValidate($request, $validateClass, $validateScene);
+            if ($result) {
+                return $result;
             }
-            $input = $request->param();
-            if ($files = $request->file()) {
-                $input += $files;
-            }
-            if (false === $v->check($input)) {
-                // todo $this->app->make($this->errorHandle);
-                $message = is_array($v->getError()) ? join(',', $v->getError()) : $v->getError();
-                return Response::create($message, 'html', 400);
-            }
-            if ($v instanceof ValidateBase) {
-                $request->withMiddleware([
-                    'allow_input_fields' => $v->getRuleKeys(),
-                ]);
-            }
-            return $next($request);
         } else {
             return $this->compatible($request, $next, $controllerClass, $controllerAction);
         }
+        return $next($request);
+    }
+
+    protected function execValidate(Request $request, string $class, string $scene): ?Response
+    {
+        /** @var Validate $validateClass */
+        $validateClass = new $class();
+        if ($scene) {
+            // 自行决定使用何种场景
+            if ('?' === $scene && $validateClass instanceof AskSceneInterface) {
+                $scene = $validateClass->askScene($request) ?: false;
+            }
+            // 选中验证场景
+            $scene && $validateClass->scene($scene);
+        }
+        $input = $request->param();
+        if ($files = $request->file()) {
+            $input += $files;
+        }
+        if (false === $validateClass->check($input)) {
+            // todo $this->app->make($this->errorHandle);
+            $message = is_array($validateClass->getError()) ? join(',', $validateClass->getError()) : $validateClass->getError();
+            return Response::create($message, 'html', 400);
+        }
+        if ($validateClass instanceof ValidateBase) {
+            $request->withMiddleware([
+                'allow_input_fields' => $validateClass->getRuleKeys(),
+            ]);
+        }
+        return null;
     }
 
     /**
@@ -124,33 +133,14 @@ class ValidateMiddleware
             if (is_string($validateCfg[0])) {
                 [$validateClass, $validateScene] = $validateCfg;
             } else {
-                [$validate_csrf, $validateClass, $validateScene] = $validateCfg;
+                [, $validateClass, $validateScene] = $validateCfg;
             }
 
             // 验证输入数据
             if ($validateClass && class_exists($validateClass)) {
-                /** @var Validate $v */
-                $v = new $validateClass();
-                if ($validateScene) {
-                    // 自行决定使用何种场景
-                    if ('?' === $validateScene && $v instanceof AskSceneInterface) {
-                        $validateScene = $v->askScene($request) ?: false;
-                    }
-                    // 选中验证场景
-                    $validateScene && $v->scene($validateScene);
-                }
-                $input = $request->param();
-                if ($files = $request->file()) {
-                    $input += $files;
-                }
-                if (false === $v->check($input)) {
-                    $message = is_array($v->getError()) ? join(',', $v->getError()) : $v->getError();
-                    return Response::create($message, 'html', 400);
-                }
-                if ($v instanceof ValidateBase) {
-                    $request->withMiddleware([
-                        'allow_input_fields' => $v->getRuleKeys(),
-                    ]);
+                $result = $this->execValidate($request, $validateClass, $validateScene);
+                if ($result) {
+                    return $result;
                 }
             }
         }
